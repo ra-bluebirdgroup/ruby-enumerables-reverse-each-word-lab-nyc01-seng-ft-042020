9c8b2caec0b2cfa81216d38fa8acca3fa5c36264
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-04 04:21:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,6 @@ def reverse_each_word(str)
   new_array = str.split(", ")
   reversed_array = []
    reversed_array = new_array.each { |sentence| sentence.reverse }
-   reversed_array
-  end
+
   reversed_array
 end
