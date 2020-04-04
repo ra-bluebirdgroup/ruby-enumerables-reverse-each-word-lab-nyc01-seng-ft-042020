c7fae9907ddf940310519574a50d4818722ef5db
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-04 04:37:33 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,7 @@
+def reverse_each_word(str)
+  new_array = str.split(" ")
+   reversed_array = []
+
+   reversed_array = new_array.collect{ |sentence| sentence.reverse }
+    reversed_array.join(" ")
+end
